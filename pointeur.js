62a8e0d25d0ba19cb0e7daa422fa53b80a13ca54
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-12-12 09:44:46 -0600

    Create pointeur.js

---
+++ pointeur.js
@@ -0,0 +1,5 @@
+var node = { 1: "root", 2:"next"};
+   var cp = node;
+   cp[1] = "decouvrir";
+   cp[2] = " A bosser";
+   console.log("node 1 : " + " " + node[1] + " " +  "node 2 " + " " +node[2]);
